349dbdefb0a3b45195f0359c47d18ff79561bfea
AlperlavKirill <kirill@alperlav.com>
2023-09-20 01:23:02 +0300

    simple behavior with quit

--- internal/generating/generator.go
+++ internal/generating/generator.go
@@ -1,4 +1,29 @@
 package generating
 
+import (
+	"aurum/internal/parcing"
+	"fmt"
+)
+
 type Generator struct {
+	nodeQuit parcing.NodeQuit
+}
+
+func NewGenerator(nodeQuit parcing.NodeQuit) Generator {
+	return Generator{nodeQuit}
+}
+
+func (g *Generator) Generate() string {
+	s := fmt.Sprintf(
+		`
+package main
+
+import "os"
+
+func main() {
+	%s
+}
+
+`, g.nodeQuit.Code())
+	return s
 }
